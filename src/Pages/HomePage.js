012f6c0089f6e4da7935575db381cd2fe1c0a33d
RheaA19 <146375653+RheaA19@users.noreply.github.com>
2025-11-11 18:42:30 +0800

    updated homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,7 +4,7 @@ import Popular from '../Components/Popular/Popular';
 import Offers from '../Components/Offers/Offers';
 import NewCollections from '../Components/NewCollections/NewCollections';
 import NewsLetter from '../Components/NewsLetter/NewsLetter';
-
+//rhea
 const HomePage = () => {
   return (
     <div>
