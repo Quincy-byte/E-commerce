f1f141badd093116b9e9a8aaca1045e46df64253
RheaA19 <146375653+RheaA19@users.noreply.github.com>
2025-11-12 03:13:16 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import ProductList from './Pages/ProductList';
-import ShopCategory from './Pages/ShopCategory';
-import LoginSignup from './Pages/LoginSignup';
-import Cart from './Pages/Cart';
-import HomePage from './Pages/HomePage';
-import Footer from './Components/Footer/Footer';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import men_banner from './Components/Assets/banner_mens.png'
-import women_banner from './Components/Assets/banner_women.png'
-import kid_banner from './Components/Assets/banner_kids.png'
+import Navbar from './Components/Navbar/Navbar';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
 
-function App() {
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
+
+const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/men' element={<ShopCategory banner={men_banner} category="Men" />} />
-        <Route path='/women' element={<ShopCategory banner={women_banner} category="Women" />} />
-        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="Kids" />} />
-        <Route path='/products' element={<ProductList />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<LoginSignup />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* FIX: Using ShopCategory for the root path to avoid the 'Home' module not found error. */}
+          <Route path='/' element={<ShopCategory />} /> 
+          
+          <Route path='/men' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
+          
+          <Route path='/product' element={<Product />}>
+            {/* The nested route is for product detail pages */}
+            <Route path=':productId' element={<Product />} />
+          </Route>
+
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 const Item = (props) => {
   return (
     <div className='item'>
-      <img src={props.image} alt={props.name} /> 
+      <Link to={`/product/${props.id}`}><img src={props.image} alt={props.name} /> </Link>
       
       <p>{props.name}</p>
       <div className="item-prices">
